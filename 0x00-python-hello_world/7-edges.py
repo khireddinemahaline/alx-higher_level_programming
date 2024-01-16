7aea3bfb46b5a68243e0341fb41804549481e179
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-01-16 19:45:43 +0000

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,4 +3,4 @@ word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"First 3 letters: {word[:3]}")
 print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[3:-2]}")
+print(f"Middle word: {word[1:-1]}")
