22641ca86b9178137a032000d5235df5e5d25a00
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-25 21:30:19 +0100

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,7 +5,7 @@
 def comb_num():
     for i in range(0, 10, 1):
         for j in range(1, 10, 1):
-            if (j >= i):
+            if (j > i):
                 print("{}{}, ".format(i, j), end = " ")
 
  
