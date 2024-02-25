2230fd4ede327818de8aba8998affec44d12a70e
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-25 18:19:13 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 """program that prints numbers from 0 to 99."""
 def print_num():
     for i in range(00,100,1):
-        print("{}, ".format(i))
+        print("{},".format(i), end=" ")
     print('\n')
 print_num();
