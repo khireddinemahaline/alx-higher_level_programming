8156b96f1d119666bb5aa8b07eb56bf2348db899
khireddinemahaline <khirozr@gmail.com>
2024-02-25 09:41:41 -0800

    6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""rogram that prints all possible different combinations of two digits"""
+
+
+def comb_num():
+    for i in range(0, 10, 1):
+        for j in range(1, 10, 1):
+            if (j >= i):
+                print("{}{}, ".format(i, j))
+
+ 
+comb_num()
