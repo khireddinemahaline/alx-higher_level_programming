ea91a438b596d50cd8f8f36bcce0f377475d92f4
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-25 18:28:35 +0100

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """program that prints numbers from 0 to 99."""
 def print_num():
+
+    
     for i in range(0, 100, 1):
         if i < 98:
             print("{:02},".format(i), end=" ")
