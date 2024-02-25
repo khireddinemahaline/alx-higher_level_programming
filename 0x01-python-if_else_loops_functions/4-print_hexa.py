f8e4abd1674e26dc7b418acf9888a49db906f8c6
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-25 18:06:02 +0100

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """prints all numbers from 0 to 98 in decimal and in hexadecimal"""
 for i in range(0, 98, 1):
-    print("{} : {}".format(i, hex(i)))
+    print("{} : {}".format(i, hex(i)), end="")
