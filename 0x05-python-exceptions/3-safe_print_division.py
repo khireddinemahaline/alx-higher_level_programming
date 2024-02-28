ee3e889ddb7fda15f5d60ff3aacd08e41f02850e
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-28 15:40:03 +0100

    3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,8 +4,9 @@
 def safe_print_division(a, b):
     try:
         res = a / b
-        print("{} / {} = {}".format(a, b, res))
+        print("{:d} / {:d} = {}".format(a, b, res))
     except:
         res = None
     finally:
         print("Inside result: {}".format(res))
+    return res
