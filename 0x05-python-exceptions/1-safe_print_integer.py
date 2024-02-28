fbb584644d28e45efa48af58f0d0af8ae767171c
khireddinemahaline <khirozr@gmail.com>
2024-02-28 05:24:21 -0800

    1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """type integer value
+    Args:
+        value (int): int number
+    Return:
+        true: if its int
+        false: otherwise
+    """
+    try:
+        print(int(value))
+        return True
+    except:
+        return False
