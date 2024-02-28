0673d023d9a7d01635376d585999641a496ba665
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-28 21:42:44 +0100

    0

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
-    return (a + b)
-def substruct(a, b):
-    return (a - b)
-def multiply(a, b):
-    return (a * b)
-def devide(a, b):
-    return (a / b)
+class calculator:
+    def add(a, b):
+        return (a + b)
+    def substruct(a, b):
+        return (a - b)
+    def multiply(a, b):
+        return (a * b)
+    def devide(a, b):
+        return (a / b)
