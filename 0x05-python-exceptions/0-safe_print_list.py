4f445a82ed8e5262cedc4a8e79c7da6671626c8c
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-28 13:54:28 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,5 +15,5 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list[:x]:
             print(i, end='')
         return x
-    except: 
-        print("Error found")
+    except IndexError: 
+        break
