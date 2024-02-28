5a9b71ec4d576a68ad48134ca98de568200b7a77
khireddinemahaline <khirozr@gmail.com>
2024-02-28 08:04:33 -0800

    5-6

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""raise function"""
+
+
+def raise_exception():
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""raise with message"""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
