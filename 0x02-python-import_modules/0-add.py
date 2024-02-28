af5fc6ce9ad178a47c0da366aad9b5cb81087c44
khireddinemahaline <khirozr@gmail.com>
2024-02-28 11:00:12 -0800

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2 
-print("{} + {} = {}".format(a, b, add(1, 2)))
+""" add tow number """
+
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(1, 2)))
