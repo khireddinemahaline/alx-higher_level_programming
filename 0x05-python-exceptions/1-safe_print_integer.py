fb21421021cd193f0b157cb76e1b38422a51a7d9
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-28 14:44:11 +0100

    1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -11,6 +11,6 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        value.is_integer()
-    except:
-        ("is not an integer")
+        return True
+    except (TypeError, ValueError):
+        return False
