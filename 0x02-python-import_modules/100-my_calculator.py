10ad6a1ffb05945a463574300f17a88b5130939d
khireddinemahaline <khirozr@gmail.com>
2024-02-29 07:20:59 -0800

    100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""prograame"""
+
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    n = len(sys.argv) - 1
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    ope = sys.argv[2]
+    print(type(b))
+    if n != 3:
+        print("Usage: ./100-my_calculator.py {} {} {}".format(a, ope, b))
+        sys.exit(1)
+    else:
+        if ope == "+":
+            print("{} {} {} = {}".format(a, ope, b, add(a, b)))
+        if ope == "-":
+            print("{} {} {} = {}".format(a, ope, b, sub(a, b)))
+        if ope == "*":
+            print("{} {} {} = {}".format(a, ope, b, mul(a, b)))
+        if ope == "/":
+            print("{} {} {} = {}".format(a, ope, b, div(a, b)))
+        sys.exit(1)
