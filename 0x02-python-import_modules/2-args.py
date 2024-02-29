7a37f4796e4a9891381b672c335fec49d02f33be
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-02-29 09:37:17 +0100

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv)
     count = n - 1
-    if count == 0:
+    if n == 0:
         print("{} arguments.".format(n))
-    elif count == 1:
-        print("{} argument:".format(x))
+    elif n == 1:
+        print("{} argument:".format(n))
     else:
         print("{} arguments:".format(n))
     for i in range(count):
