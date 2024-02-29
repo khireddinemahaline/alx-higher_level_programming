11aaefa452110f4b3427f64549e40feb6640fda9
khireddinemahaline <khirozr@gmail.com>
2024-02-29 06:03:31 -0800

    4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""program that prints all the names defined by the compiled module"""
+
+
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
