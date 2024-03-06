d61aee131949cae469e9e56680c9780dd6eca1e1
khireddinemahaline <khirozr@gmail.com>
2024-03-06 08:56:07 -0800

    100

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""pritn a pwoer of b"""
+
+
+def pow(a, b):
+    return a**b 
---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""print from 1 to 100"""
+
+
+def fizzbuzz():
+    for i in range(101):
+        print(i, end=' ')
