8d89210258f614841f2b3000bb8491715bbcfb1a
khireddinemahaline <khirozr@gmail.com>
2024-03-14 03:25:57 -0700

    3

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a function that prints all integers of a list."""
+
+
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]), end='\n')
