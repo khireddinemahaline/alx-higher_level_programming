1d15aa8605316ad77a7efac737b69536c103a6b3
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-03-28 14:32:39 +0100

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- auto generated primary key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE hbtn_0d_usa.states (
-	id INT UNIQUE DEFAULT (uuid()) NOT NULL PRIMARY KEY,
+	id INT UNIQUE DEFAULT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 	);
