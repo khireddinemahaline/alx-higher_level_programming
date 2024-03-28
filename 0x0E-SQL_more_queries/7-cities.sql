15145fcb69a845593027ca536820d6462eca6af1
khireddinemahaline <khirozr@gmail.com>
2024-03-28 06:57:30 -0700

    sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT NOT NULL UNIQUE AUTO_INCEMENT PRIMARY KEY,
+	state_id INT NOT NULL FORGEIGN KEY(id),
+	name VARCHAR(256) NOT NULL
+	);
