7425786f15c2717dc9d01943eaf7c56f4786f851
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-03-28 15:15:22 +0100

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL UNIQUE AUTO_INCEMENT PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FORGEIN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
  	);
 FLUSH PRIVILEGES;
