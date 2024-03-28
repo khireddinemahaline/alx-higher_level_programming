c74949e75d066183970feff74d5720121c5a71a0
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-03-28 16:11:37 +0100

    sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database
 SELECT cities.id, cities.name, states.name FROM cities
 JOIN states
-ON states.id = cities.states_id
+ON cities.states_id = states.id
 ORDER BY cities.id;
