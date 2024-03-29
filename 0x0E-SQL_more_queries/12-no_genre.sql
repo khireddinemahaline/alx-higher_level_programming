e97f39880cbbe0a702097c3b153a8e064792601c
khireddine mhalaine <64756029+khireddinemahaline@users.noreply.github.com>
2024-03-29 08:36:55 +0100

    sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 --  lists all shows no gener
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.id = NULL
+WHERE tv_show_genres.id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
