d135bc8bfd6a961a307ba082b19389f24952cad5
khireddinemahaline <khirozr@gmail.com>
2024-04-23 18:32:31 +0100

    3,4

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
     console.log('Not a number');
 } else if (Number.isInteger(process.argv[2]) === false) {
-    console.log(parseInt(process.argv[2]));
+    console.log(parseInt('My number:', process.argv[2]));
 }
