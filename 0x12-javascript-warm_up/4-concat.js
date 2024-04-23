9cd52a0aea6f3faefc40a8f310212e840eb27379
khireddinemahaline <khirozr@gmail.com>
2024-04-23 18:11:47 +0100

    3,4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,4 +4,4 @@ if (process.argv[2] === undefined) {
 } else if (process.argv[3] === undefined) {
     process.argv[3] = undefined;
 }
-console.log(process.argv[2], 'is' , process.argv[3]);
+console.log(process.argv[2], 'is' , process.argv[3])
