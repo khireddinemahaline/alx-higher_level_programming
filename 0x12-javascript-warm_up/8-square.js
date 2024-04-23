2baf17d5cd34d178519afc46cdfed17ec07de218
khireddinemahaline <khirozr@gmail.com>
2024-04-23 19:19:29 +0100

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,7 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   let i = 0;
-  let j = 0;
-  x = parseInt(process.argv[2]);
+  x = Number(process.argv[2]);
   while (i < x) {
     console.log('x'.repeat(x));
     i++;
