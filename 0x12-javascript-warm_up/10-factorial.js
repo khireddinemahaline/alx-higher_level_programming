d847b0a86c5712ae1110d5e2648711449df301c7
khireddinemahaline <khirozr@gmail.com>
2024-04-24 07:13:22 +0100

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 function factorial (x) {
   if (isNaN(x) || x === 0) {
-    return (1);
+    return 1;
   } else {
-    return (x * factorial(x - 1));
+    return x * factorial(x - 1);
   }
 }
 console.log(factorial(process.argv[2]));
-   
