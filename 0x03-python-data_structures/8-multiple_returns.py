9421a322f5f436197f318f489d2265d5f668a0ad
khireddinemahaline <khirozr@gmail.com>
2024-07-23 11:49:57 -0400

    py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,10 @@
 """
 multiple return
 """
+
+
 def multiple_returns(sentence):
-    if len(sentence) < 0:
+    if len(sentence) <= 0:
         return None
     else:
         tuple_sen = len(sentence), sentence[0]
